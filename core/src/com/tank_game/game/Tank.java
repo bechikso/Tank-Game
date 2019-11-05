b870f589d263dd9fe359acf9cd56b95e804c2887
Ben The Beast <benech5@gmail.com>
2019-11-05 15:47:30 -0500

    Added canon that rotates to point to mouse.

--- core/src/com/tank_game/game/Tank.java
+++ core/src/com/tank_game/game/Tank.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
@@ -36,7 +37,9 @@ public class Tank extends ApplicationAdapter {
         // Draws all the textures in the game
         batch.begin();
         batch.draw(tankImg, collision.x, collision.y);
-        batch.draw(canonImg, collision.x + 24, collision.y + 24);
+        batch.draw(canonImg, collision.x + 24, collision.y + 24, 8, 8, 16, 64,
+        1, 1, mouse_point(), 0, 0, 16, 64, false, false);
+
         batch.end();
     }
 
@@ -52,12 +55,22 @@ public class Tank extends ApplicationAdapter {
         if(collision.x > 800 - 64) collision.x = 800 - 64;
     }
 
-    public void mouse_point() {
+    public float mouse_point() {
         // Holds the mouse coordinates
         Vector3 mousePos = new Vector3();
         if (timer == 50) {
-            System.out.println("x: " + Gdx.input.getX() + ", y: " + Gdx.input.getY());
+            System.out.println("x: " + collision.x + ", y: " + collision.y);
+            System.out.println("Mouse x: " + Gdx.input.getX() + ", Mouse y: " + Gdx.input.getY());
             timer = 0;
         }
+
+        float xDif = collision.x - Gdx.input.getX();
+        float yDif = collision.y - Gdx.input.getY();
+        double angle = Math.toDegrees(Math.atan(xDif / yDif));
+        if (yDif <= 0) {
+            angle += 180;
+        }
+        // System.out.println(angle);
+        return (float)angle;
     }
 }
